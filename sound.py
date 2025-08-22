0113822c71c55da61eaf9554edc096114939d7da
keerthivnair <keerthivnair2005@gmail.com>
2025-08-22 14:34:48 +0530

    first commit

---
+++ sound.py
@@ -0,0 +1,38 @@
+import sounddevice as sd
+import numpy as np
+import time
+
+
+DURATION = 1  
+FS = 44100     
+THRESHOLD = 70 
+
+print("Noise Monitoring System (Real Microphone Input)\n")
+print("Press Ctrl+C to stop\n")
+
+def get_db_level(data):
+    """Convert audio signal to decibel level"""
+    rms = np.sqrt(np.mean(data**2))  # calc the rms 
+    db = 20 * np.log10(rms + 1e-6)  + 60  
+    return db
+
+try:
+    while True:
+
+        recording = sd.rec(int(DURATION * FS), samplerate=FS, channels=1, dtype='float64')
+        sd.wait()  
+
+        
+        db_level = get_db_level(recording)
+
+        
+        print(f"Current Noise Level: {db_level:.2f} dB", end=" -> ")
+        if db_level > THRESHOLD:
+            print("🚨 RED LIGHT ON")
+        else:
+            print("✅ RED LIGHT OFF")
+
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("\nStopped monitoring.")
