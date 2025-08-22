5293cb7aa199a25d3e90ecb2ec96e5661cebcaf8
keerthivnair <keerthivnair2005@gmail.com>
2025-08-22 22:55:02 +0530

    added backend

--- sound.py
+++ sound.py
@@ -1,19 +1,26 @@
 import sounddevice as sd
 import numpy as np
 import time
+import paho.mqtt.client as mqtt
 
 
 DURATION = 1  
 FS = 44100     
-THRESHOLD = 70 
+THRESHOLD = -7
+
+broker = "localhost"
+topic = "traffic/noise"
+
+client = mqtt.Client()
+client.connect(broker)
 
 print("Noise Monitoring System (Real Microphone Input)\n")
 print("Press Ctrl+C to stop\n")
 
 def get_db_level(data):
     """Convert audio signal to decibel level"""
-    rms = np.sqrt(np.mean(data**2))  # calc the rms 
-    db = 20 * np.log10(rms + 1e-6)  + 60  
+    rms = np.sqrt(np.mean(data**2)) 
+    db = 20 * np.log10(rms + 1e-6)   
     return db
 
 try:
@@ -27,10 +34,19 @@ try:
 
         
         print(f"Current Noise Level: {db_level:.2f} dB", end=" -> ")
+
+        status = "HIGH NOISE" if db_level > THRESHOLD else "NORMAL NOISE"
+
+        payload = f'{{"db": {db_level:.2f}, "status": "{status}"}}'
+
+        client.publish(topic,payload)  
+
+        # print("Published",payload)  
+        
         if db_level > THRESHOLD:
             print("🚨 RED LIGHT ON")
         else:
-            print("✅ RED LIGHT OFF")
+            print("✅ NORMAL")
 
         time.sleep(0.5)
 
